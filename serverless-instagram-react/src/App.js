b00050e72df07e3aa5c17e860cc84223819498a2
Gabriel Falcão <gabriel@nacaolivre.org>
2018-02-08 00:40:50 -0200

    Finishes basic css layout based on bootstrap 4.0.0
    Also Includes:
    
    - Open-Iconic icons with bootstrap support
    - React-Dropzone for easy uploads

--- serverless-instagram-react/src/App.js
+++ serverless-instagram-react/src/App.js
@@ -1,23 +1,49 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar'
-import 'bootstrap/dist/css/bootstrap.css'
+import Dropzone from 'react-dropzone'
+
+import popcorn from './popcorn.png'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <Navbar />
+    constructor(props) {
+        super(props);
+        this.state = {
+            previewImage: null
+        };
+    }
+    onDrop(acceptedFiles, rejectedFiles) {
+        if (acceptedFiles.length > 0) {
+            let image = acceptedFiles[0];
+            this.setState({
+                previewImage: image.preview
+            })
+        }
+        console.log("Accepted Files", acceptedFiles);
+    }
+    render() {
+        return (
+            <div className="App">
+                <Navbar />
+                <div className="jumbotron">
+                    <div className="container">
+                        <img src={popcorn} alt="popcorn" height="128" className="float-left" />
+                        <h1>All videos. All the time.</h1>
+                        <p>Guaranteed 100% server free.</p>
+                    </div>
+                </div>
 
-          <div className="jumbotron">
-              <div id="hero" className="container">
-                  <h1>All videos. All the time.</h1>
-                  <p>Guaranteed 100% server free.</p>
-              </div>
-          </div>
-
-      </div>
-    );
-  }
+                <div className="container">
+                    <Dropzone className="mx-auto d-block" onDrop={this.onDrop.bind(this)} accept="video/*,image/*">
+                        <p style={{height: "300px", backgroundColor: "#CCC"}}>Drag-and-drop your image or video here...</p>
+                    </Dropzone>
+                    {this.state.previewImage != null ? <img src={this.state.previewImage} alt="preview" /> : null}
+                    {/* <button className="btn btn-info ">
+                        <span class="oi oi-plus"></span>
+                        </button> */}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- serverless-instagram-react/src/components/Navbar.js
+++ serverless-instagram-react/src/components/Navbar.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-inverse">
+            <nav className="navbar navbar-dark bg-dark">
                 <div className="container-fluid">
                     <div className="navbar-header">
                         <a className="navbar-brand" href="/">
